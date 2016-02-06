dda3ed0c034087c137b6ab31e76a22aa519d5c29
Huseyin Sekmenoglu <hus_as@yahoo.com>
2016-02-06 11:26:17 +0200

    06.02.2016: added mvc tester

---
+++ TesterMvc/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using hsFunctions;
+
+namespace TesterMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.encrypted = functions.Encrypt("Hüseyin Sekmenoğlu");
+            ViewBag.decrypted = functions.Decrypt(ViewBag.encrypted);
+            return View();
+        }
+    }
+}
